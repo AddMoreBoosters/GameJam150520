077c3481986a7dcc241b9b5986576a640956a0b9
Herman Tandberg Dybing <hermantd@stud.ntnu.no>
2020-05-15 11:24:02 +0200

    Started working on some platformer controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _playerRB;
+    private CapsuleCollider _playerCollider;
+    private bool _canJump = false;
+    [SerializeField]
+    private float _coyoteTime = 0.2f;
+
+    private float _horizontalInput;
+    private float _verticalInput;
+    private float _rotationHorizontalInput;
+    private float _rotationVerticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRB = gameObject.GetComponent<Rigidbody>();
+        _playerCollider = gameObject.GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  Get input
+        //  Rotate
+        //  Move
+        //  Jump if able
+    }
+
+    private void GetInput ()
+    {
+
+    }
+}
