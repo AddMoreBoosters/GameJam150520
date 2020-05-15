69dd3e3476c02d78daaf8954b329164b9b1fbc0e
Herman Tandberg Dybing <hermantd@stud.ntnu.no>
2020-05-15 11:50:37 +0200

    Added jumping and coyote time

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,11 @@ public class PlayerController : MonoBehaviour
     private Rigidbody _playerRB;
     private CapsuleCollider _playerCollider;
     private bool _canJump = false;
+    private float _elapsedJumpTime = 0f;
     [SerializeField]
     private float _coyoteTime = 0.2f;
+    [SerializeField]
+    private float _jumpSpeed = 5f;
 
     private float _horizontalInput;
     private float _verticalInput;
@@ -25,14 +28,48 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //  Get input
+        GetInput();
+
         //  Rotate
         //  Move
-        //  Jump if able
+
+        CheckAbleToJump();
+        if (Input.GetKeyDown(KeyCode.Space) && _canJump == true)
+        {
+            _playerRB.velocity = new Vector3(_playerRB.velocity.x, _jumpSpeed, _playerRB.velocity.z);
+        }
     }
 
     private void GetInput ()
     {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+        _rotationHorizontalInput = Input.GetAxis("Mouse X");
+        _rotationVerticalInput = Input.GetAxis("Mouse Y");
+    }
 
+    private void CheckAbleToJump ()
+    {
+        Vector3 bottom = new Vector3(_playerCollider.bounds.center.x, _playerCollider.bounds.min.y - 0.1f, _playerCollider.bounds.center.z);
+
+        //  Check if the bottom of the collider is touching something
+        if (Physics.CheckCapsule(_playerCollider.bounds.center, bottom, _playerCollider.radius) == true)
+        {
+            _canJump = true;
+            _elapsedJumpTime = 0f;
+            return;
+        }
+
+        //  If we are not touching something, but we were before, activate coyote time
+        if (_canJump == true)
+        {
+            if (_elapsedJumpTime > _coyoteTime)
+            {
+                _canJump = false;
+                return;
+            }
+
+            _elapsedJumpTime += Time.deltaTime;
+        }
     }
 }
