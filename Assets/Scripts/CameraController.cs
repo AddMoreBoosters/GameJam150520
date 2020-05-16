5e7862a37af2afa1bb487bfb512bc85f3737db20
Herman Tandberg Dybing <hermantd@stud.ntnu.no>
2020-05-16 15:30:55 +0200

    Added camera controller, camera is no longer parented to player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _playerTransform;
+    private Vector3 _offset;
+
+    public bool followPlayer = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _offset = transform.position - _playerTransform.position;
+    }
+
+    private void LateUpdate()
+    {
+        if (followPlayer == true)
+        {
+            transform.position = _playerTransform.position + _offset;
+        }
+    }
+}
